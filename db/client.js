c3aa339de1f952868365d07478be4f57fff6f70f
Ryan Riley Puzon <rrgpuzon@gmail.com>
2025-04-03 19:08:29 -0700

    jio

--- db/client.js
+++ db/client.js
@@ -1,8 +1,15 @@
 const { Client } = require("pg");
-const { CONNECTION_STRING } = process.env;
+const { DB_USER, DB_HOST, DB_PASSWORD, DB_DATABASE, DB_PORT } = process.env;
 
 const client = new Client({
-  connectionString: CONNECTION_STRING,
+  user: DB_USER,
+  host: DB_HOST,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+  port: DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = client;
